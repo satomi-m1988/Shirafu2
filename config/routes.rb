c40fd873a351527c439445e6d034fc3e7f4e6844
vagrant <vagrant@localhost.localdomain>
2020-09-20 03:47:59 +0000

    fix routeing & SignUp view file

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  get 'tweets/new'
-  get 'tweets/create'
-  get 'tweets/index'
-  get 'tweets/show'
-  get 'tweets/edit'
-  get 'tweets/update'
-  get 'tweets/destroy'
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
+  # get 'tweets/new'
+  # get 'tweets/create'
+  # get 'tweets/index'
+  # get 'tweets/show'
+  # get 'tweets/edit'
+  # get 'tweets/update'
+  # get 'tweets/destroy'
+  # get 'users/index'
+  # get 'users/show'
+  # get 'users/edit'
+  # get 'users/update'
   devise_for :users
+  resources :tweets
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
