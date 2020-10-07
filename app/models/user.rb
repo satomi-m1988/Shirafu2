0b386be7d764750670d79d533bf587a28cdb9904
“Satomi <satomi2458@yahoo.co.jp>
2020-10-07 13:48:59 +0900

    Add image area

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  attachment :image
   has_many :tweets, dependent: :destroy
 end
